278b85ef3987605a9fec6bf62499b4af2de20a83
Boris Vujicic <turshija@gmail.com>
2012-11-22 17:36:00 +0100

    Init

---
+++ gmail.php
@@ -0,0 +1,75 @@
+<?php
+$icons[] = array(
+    'name' => 'GameTracker.rs',
+    'url' => 'http://www.gametracker.rs/user_info/1/turshija/',
+    'iconUrl' => 'http://www.turshija.com/signature/gtrs.png'
+);
+$icons[] = array(
+    'name' => 'Twitter',
+    'url' => 'http://www.twitter.com/turshija',
+    'iconUrl' => 'http://www.turshija.com/signature/twitter.png'
+);
+$icons[] = array(
+    'name' => 'LinkedIn',
+    'url' => 'http://rs.linkedin.com/pub/boris-vujicic/36/437/81a',
+    'iconUrl' => 'http://www.turshija.com/signature/linkedin.png'
+);
+$icons[] = array(
+    'name' => 'Google Plus',
+    'url' => 'https://plus.google.com/106157635008740201698/posts',
+    'iconUrl' => 'http://www.turshija.com/signature/googleplus.png'
+);
+?>
+______________________________________________
+<div style="font-size: 13px; font-family: Verdana, Arial, Helvetica, sans-serif;">
+    <div style="margin: 0 0 8px 0">
+        <table border="0">
+            <tbody>
+            <tr valign="top">
+                <td>
+                    <img style="width:70px;" src="http://www.turshija.com/signature/boris.png" alt="logo">
+                    <span style="font-size: small;">&nbsp; <br></span>
+                </td>
+                <td style="text-align: left;">
+                    <span style="font-size: small;">
+                        <span style="color: #808080;">
+                            <strong>Boris Vujicic</strong>
+                            <span style="color: #888888;">
+                                <em>Lead web developer</em>
+                            </span>
+                        </span>
+                    </span>
+                    <br>
+                    <div style="margin-top: 0px; margin-bottom: 0px;">
+                        <span style="font-size: small;">
+                            <span style="color: #808080;">
+                                <span style="color: #ff6600;"> | </span>
+                                Email: turshija@gmail.com
+                            </span>
+                        </span>
+                    </div>
+                    <span style="font-size: small;">
+                        <span style="color: #666699;">http://www.turshija.com<br></span>
+                    </span>
+                    <span style="font-size: small;">
+                        <span style="color: #666699;">http://www.kgb-hosting.com<br></span>
+                    </span>
+                    <span style="font-size: small;">
+                        <span style="color: #666699;">http://www.gametracker.rs<br></span>
+                    </span>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div style="clear:both"></div>
+    </div>
+    <?php foreach ($icons as $icon): ?>
+        <a target="_blank" rel="nofollow" href="<?php echo $icon['url'] ?>" style=""><img width="16" height="16" alt="<?php echo $icon['name'] ?>"  border="0" src="<?php echo $icon['iconUrl'] ?>"></a>
+    <?php endforeach ?>
+    <br />
+    <span style="color: gray;">Contact me: </span>
+    <img width="16" height="16" alt="Google Talk" style="padding: 0px 0px 5px 0px; vertical-align: middle;" border="0" src="http://www.turshija.com/signature/gtalk.png">
+    turshija@gmail.com
+    <img width="16" height="16" alt="Skype" style="padding: 0px 0px 5px 0px; vertical-align: middle;" border="0" src="http://www.turshija.com/signature/skype.png">
+    turshija<br>
+</div>
